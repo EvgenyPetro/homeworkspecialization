cb823e0103afda067ff73a8fe7cd697fd2d2cd0d
Evgeny Petrov <petroves88@mail.ru>
2022-12-03 00:00:34 +0300

    write algoritm

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+string[] arr1 = { "hello", "2", "world", ":-)" };
+string[] arr2 = { "1234", "1567", "-2", "computer sience" };
+string[] arr3 = { "Russia", "Denmark", "Kazan" };
+
+string[] GetNewArray(String[] array)
+{
+    string[] tempArray = new string[array.Length];
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            tempArray[temp] = array[i];
+            temp++;
+        }
+    }
+
+    string[] resultArray = new string[temp];
+    for (int i = 0; i < temp; i++)
+    {
+        resultArray[i] = tempArray[i];
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+
+PrintArray(GetNewArray(arr1));
+PrintArray(GetNewArray(arr2));
+PrintArray(GetNewArray(arr3));
